579effeb64a70ccc1c86aab8a477944ead7ba42d
ajkarczewski <karczewskiamanda@gmail.com>
2022-08-30 16:48:56 -0500

    feat: set up routes for main page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter,
+         Routes,
+         Route,
+       } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+// import Expenses from "./routes/expenses";
+import About from "./routes/about";
+import ArtDesign from './routes/artdesign';
+import UxUiWork from './routes/uxui';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<App />} />
+    <Route path="/" element={<About />} />
+    <Route path="/ArtDesign" element={<ArtDesign />} />
+    <Route path="/UxUiWork" element={<UxUiWork />} />
+  </Routes>
+</BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
